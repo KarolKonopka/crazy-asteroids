fca061a4362313e36e4e281b5009ac03e36b2d23
KarolKonopka <KarolKonopka00@gmail.com>
2018-11-14 19:26:01 +0100

    SFML configuration

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
+#define WIDTH 1280
+#define HEIGHT 720
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main(void) {
+int main() {
 	std::cout << "Crazy Asteroids, author: Karol Konopka\n";
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT),"Crazy Asteroids!");
+	while(window.isOpen()){
+		sf::Event event;while(window.pollEvent(event)){
+			if(event.type == sf::Event::Closed) window.close();
+			if(event.type == sf::Event::KeyPressed) printf("Button pressed\n");
+			if(event.type == sf::Event::Resized){
+				sf::Vector2u size(WIDTH, HEIGHT);
+				window.setSize(size);
+			}
+		}
+		window.clear();
 
-	getchar();
+		window.display();
+	}
 	return 0;
 }
